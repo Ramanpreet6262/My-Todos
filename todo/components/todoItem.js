e109dd5174f8b2df030d383b5ec5c2fc9609f5f0
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-12-04 18:01:34 +0530

    updated css

--- todo/components/todoItem.js
+++ todo/components/todoItem.js
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
     marginTop: 16,
     borderColor: '#bbb',
     borderWidth: 1,
-    borderStyle: 'dashed',
+    borderStyle: 'solid',
     borderRadius: 10
   }
 });
