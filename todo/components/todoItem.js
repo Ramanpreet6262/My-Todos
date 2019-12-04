9f4e4a5f00f5a12618da3aa22fdc1c7a7f9c06d3
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-12-04 17:49:37 +0530

    added todoitem component

---
+++ todo/components/todoItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+// if we write props in argument, we will access it with props.item
+// Or we can use destructuring and use { item } in argument itself....
+export default function TodoItem({ item }) {
+  return (
+    <TouchableOpacity>
+      <Text style={styles.item}>{item.text}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  item: {
+    padding: 16,
+    marginTop: 16,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderRadius: 10
+  }
+});
