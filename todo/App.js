e2b55a90c9b97bc4d502c61199f01a905e6f551b
Ramanpreet6262 <ramanpreet6262@gmail.com>
2019-12-07 20:23:54 +0530

    added alert to show validation error

--- todo/App.js
+++ todo/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
@@ -22,6 +22,10 @@ export default function App() {
       setTodos(prevTodos => {
         return [{ text: text, key: Math.random().toString() }, ...prevTodos];
       });
+    } else {
+      Alert.alert('OOPS!', 'Todos must be over 3 chars long', [
+        { text: 'Understood', onPress: () => console.log('alert closed!!') }
+      ]);
     }
   };
 
